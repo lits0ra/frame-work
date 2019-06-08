c70a7c3a88de8703661a80ca4bed146fdfa472ba
sora <s0ra_kanda@icloud.com>
2019-06-08 19:57:05 +0900

    パラメーターを取得できるようにした

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 require './simple_app'
+
 get '/name' do
+  @sssss = 11
+  erb("./index.erb")
+end
+
+get '/user/:id/hello' do
   @sssss = 11
   erb("./index.erb")
 end
--- simple_app.rb
+++ simple_app.rb
@@ -1,12 +1,39 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'tilt'
 
 class SimpleApp
   HASH = {}
-  @@not_found_response = "<h1>404 Not found</h1>"
+  PARAMS = {}
+  @@not_found_response = '<h1>404 Not found</h1>'
+  def self.check_path(request_path, hash_key)
+    request_path_list = request_path.split('/')
+    hash_keys_list = hash_key[0].split('/')
+    request_path_list.zip(hash_keys_list) do |x, y|
+      if x != ''
+        if x != y
+          if y.start_with?(':')
+            y = y.sub(/:/, '')
+            PARAMS.store(y, x)
+          else
+            return false
+          end
+        end
+      end
+    end
+    return true
+  end
+
   def call(env)
-    request_path = env['REQUEST_PATH']
-    block = HASH[request_path]
+    request_path = []
+    request_path.push(env['REQUEST_PATH'])
+    block = nil
+    HASH.each do |hash|
+      request_path.each do |request_path|
+        block = HASH[hash[0]] if SimpleApp.check_path(request_path, hash)
+      end
+    end
     if block.nil?
       return [
         200,
@@ -19,6 +46,10 @@ class SimpleApp
   end
 end
 
+def params
+  SimpleApp::PARAMS
+end
+
 def get(route, &block)
   SimpleApp::HASH.store(route, block)
 end
@@ -29,10 +60,7 @@ end
 
 def compile_template(name, format)
   tiltEngine = Tilt[name]
-  if tiltEngine.nil?
-    raise ".#{format} template engine not found!"
-  end
-  # template = File.open(name, 'r', &:read)
+  raise ".#{format} template engine not found!" if tiltEngine.nil?
   erb_engin = Tilt.new(name)
   text(erb_engin.render)
 end
